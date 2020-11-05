200206bad90a8f8f87ba6b77953436ae5f8ee4ca
szaff <sb782396771@163.com>
2020-11-05 21:38:48 +0800

    增加recommend game部分的切换动画

--- FrontEnd/src/script/Store.js
+++ FrontEnd/src/script/Store.js
@@ -1,63 +1,113 @@
+/**
+ * 登陆功能
+ */
+
 function login() {
     window.location.href = "Login.html";
     //todo: 若已经登陆，则不显示登陆按钮，显示用户头像
 }
 
+/**
+ * 主页面搜索功能
+ */
 function search(){
     //todo: 向服务器发出请求，并跳转到搜索结果页面
     return;
 }
 
 
+/**
+ * 对game_recommend模块进行图片切换
+ */
 //todo: 把此功能用Vue轮播组件重写
-let GAME_RECOMMEND_SLIDE_COUNTER = 0;// 我知道定义全局变量不优雅，但我找不到其他方法了，有好的优化的话务必告诉我😂
+let GAME_RECOMMEND_CURR_SLIDE = 0;// 我知道定义全局变量不优雅，但我找不到其他方法了，有好的优化的话务必告诉我😂
+let GAME_RECOMMEND_NEXT_SLIDE = 1;
+let IS_SLIDE0_ONSHOW = true;// slide0正在展示则为true，slide1正在展示则为false
+let GAME_RECOMMEND_INTERVAL = 0;
 function game_recommend_slide(){
-    //todo: 将images改为从服务器获取图片地址
     let images = [
         '../../figures/images/AnimalCrossing.jpg',
         '../../figures/images/Cyberpunk2077.jpg',
         '../../figures/images/LegendOfZelda.jpg',
         '../../figures/images/RedDeadRedemption2.jpg'
     ];
+    let slide = [document.getElementById("img_content_1"), document.getElementById("img_content_2")];
     let div_dots = document.getElementById("div_dots");
     let dot = [];
+    let dots = div_dots.children;
     for (let i = 0; i < images.length; i ++){
         dot[i] = document.createElement("div");
     }
-
     for (let i = 0; i < images.length; i ++){
         div_dots.appendChild(dot[i]);
         div_dots.children[i].className = "dot";
     }
-    let dots = div_dots.children;
+    /* todo: 有bug，点击以后无法正确地clearInterval
     for (let i = 0; i < dots.length; i ++){
-        dots[i].addEventListener("onclick", function (){
-            GAME_RECOMMEND_SLIDE_COUNTER = i;
-            curr_img.src = images[GAME_RECOMMEND_SLIDE_COUNTER];
+        dot[i].addEventListener("click", function(){
+            clearInterval(GAME_RECOMMEND_INTERVAL);
+            console.log(GAME_RECOMMEND_INTERVAL);
+            for (let x = 0; x < dots.length; x ++)
+                dots[x].style.backgroundColor = "#808080";
+            dots[i].style.backgroundColor = "#d3d3d3";
+            GAME_RECOMMEND_CURR_SLIDE = i;
+            GAME_RECOMMEND_NEXT_SLIDE = i===images.length-1? 0 : i+1;
+            IS_SLIDE0_ONSHOW = true;
+            slide[0].src = images[GAME_RECOMMEND_CURR_SLIDE];
+            slide[1].src = images[GAME_RECOMMEND_NEXT_SLIDE];
+            slide[0].style.opacity = "1.0";
+            slide[1].style.opacity = "0.0";
+            GAME_RECOMMEND_INTERVAL = setInterval(image_change, 4000);
         });
-    }
-
-    let curr_img = document.getElementById("img_content");
-    image_change();
-    window.setInterval(image_change, 4000);
+    }*/
+    slide[0].src = images[GAME_RECOMMEND_CURR_SLIDE];
+    slide[1].src = images[GAME_RECOMMEND_NEXT_SLIDE];
+    slide[1].style.opacity = "0.0";
+    dots[GAME_RECOMMEND_CURR_SLIDE].style.backgroundColor = "#d3d3d3";
+    GAME_RECOMMEND_INTERVAL = setInterval(image_change, 4000);
+    console.log(GAME_RECOMMEND_INTERVAL);
 }
-function image_change(){
-    //todo: 将images改为从服务器获取图片地址
+
+function image_change() {
     let images = [
         '../../figures/images/AnimalCrossing.jpg',
         '../../figures/images/Cyberpunk2077.jpg',
         '../../figures/images/LegendOfZelda.jpg',
         '../../figures/images/RedDeadRedemption2.jpg'
     ];
-    let dots = document.getElementById("div_dots").children;
-    let curr_img = document.getElementById("img_content");
-    curr_img.src = images[GAME_RECOMMEND_SLIDE_COUNTER];
-    dots[GAME_RECOMMEND_SLIDE_COUNTER].style.backgroundColor = "#d3d3d3";
-    let index = GAME_RECOMMEND_SLIDE_COUNTER-1===-1? images.length-1 : GAME_RECOMMEND_SLIDE_COUNTER-1;
-    dots[index].style.backgroundColor = "#808080";
-    GAME_RECOMMEND_SLIDE_COUNTER++;
-    if (GAME_RECOMMEND_SLIDE_COUNTER === images.length) GAME_RECOMMEND_SLIDE_COUNTER = 0;
+    let slide = [document.getElementById("img_content_1"), document.getElementById("img_content_2")];
+    let div_dots = document.getElementById("div_dots");
+    let dots = div_dots.children;
+
+    dots[GAME_RECOMMEND_CURR_SLIDE].style.backgroundColor = "#808080";
+    dots[GAME_RECOMMEND_NEXT_SLIDE].style.backgroundColor = "#d3d3d3";
+
+    anime({
+        targets: slide,
+        opacity: function (el, i, l) {
+            if (IS_SLIDE0_ONSHOW) {
+                if (i === 0) return "0.0"
+                else return "1.0"
+            } else {
+                if (i === 0) return "1.0"
+                else return "0.0"
+            }
+        },
+        easing: 'easeInOutQuad'
+    });
+    setTimeout(function(){
+        IS_SLIDE0_ONSHOW = !IS_SLIDE0_ONSHOW;
+        GAME_RECOMMEND_CURR_SLIDE = GAME_RECOMMEND_NEXT_SLIDE;
+        GAME_RECOMMEND_NEXT_SLIDE ++;
+        if (GAME_RECOMMEND_NEXT_SLIDE === images.length) GAME_RECOMMEND_NEXT_SLIDE = 0;
+        if (IS_SLIDE0_ONSHOW){
+            slide[1].src = images[GAME_RECOMMEND_NEXT_SLIDE];
+        }else {
+            slide[0].src = images[GAME_RECOMMEND_NEXT_SLIDE];
+        }
+    }, 3900);
 }
+
 
 
 
